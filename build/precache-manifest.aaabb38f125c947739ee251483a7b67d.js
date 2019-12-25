6dcab1565eca7ae84857126678234774dd159a0c
student1311 <1311@i.ua>
2019-12-25 13:45:16 +0200

    first commit

---
+++ build/precache-manifest.aaabb38f125c947739ee251483a7b67d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc690c91c44ba7d52f602e88fdc03a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99df7dd6d988f586bd19",
+    "url": "/static/css/main.95efb7e4.chunk.css"
+  },
+  {
+    "revision": "7f1a89f98d87d71929f4",
+    "url": "/static/js/2.5e0842e6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5e0842e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99df7dd6d988f586bd19",
+    "url": "/static/js/main.558cd1d2.chunk.js"
+  },
+  {
+    "revision": "215b67208eb198e9fe36",
+    "url": "/static/js/runtime-main.9a605b21.js"
+  }
+]);
